02cb97466c345f6087d6d5800fb6026aadf21169
jhs512 <jangka512@gmail.com>
2020-08-06 12:32:59 +0900

    매 요청에 필요한 유용한 정보를 인터셉테에서 request에 태운다.

--- src/main/java/com/sbs/jhs/at/interceptor/BeforeActionInterceptor.java
+++ src/main/java/com/sbs/jhs/at/interceptor/BeforeActionInterceptor.java
@@ -1,5 +1,10 @@
 package com.sbs.jhs.at.interceptor;
 
+import java.net.URLEncoder;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -9,6 +14,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sbs.jhs.at.dto.Member;
 import com.sbs.jhs.at.service.MemberService;
 
@@ -25,9 +31,48 @@ public class BeforeActionInterceptor implements HandlerInterceptor {
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 
+		// 기타 유용한 정보를 request에 담는다.
+		Map<String, Object> param = new HashMap<>();
+
+		Enumeration<String> parameterNames = request.getParameterNames();
+
+		while (parameterNames.hasMoreElements()) {
+			String paramName = parameterNames.nextElement();
+			Object paramValue = request.getParameter(paramName);
+
+			param.put(paramName, paramValue);
+		}
+
+		ObjectMapper mapper = new ObjectMapper();
+		String paramJson = mapper.writeValueAsString(param);
+
+		String requestUri = request.getRequestURI();
+		String queryString = request.getQueryString();
+
+		String requestUriQueryString = requestUri;
+		if (queryString != null && queryString.length() > 0) {
+			requestUriQueryString += "?" + queryString;
+		}
+
+		String encodedRequestUriQueryString = URLEncoder.encode(requestUriQueryString, "UTF-8");
+
+		request.setAttribute("requestUriQueryString", requestUriQueryString);
+		request.setAttribute("urlEncodedRequestUriQueryString", encodedRequestUriQueryString);
+		request.setAttribute("param", param);
+		request.setAttribute("paramJson", paramJson);
+
+		boolean isAjax = requestUri.endsWith("Ajax");
+
+		if (isAjax == false) {
+			if (param.containsKey("ajax") && param.get("ajax").equals("Y")) {
+				isAjax = true;
+			}
+		}
+
+		request.setAttribute("isAjax", isAjax);
+
 		// 설정 파일에 있는 정보를 request에 담는다.
 		request.setAttribute("logoText", this.siteName);
-
 		HttpSession session = request.getSession();
 
 		// 임시작업
